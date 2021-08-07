5ae90ec4613ff2f3f5ec749fddeca3f77bbb0a0d
Halis Bay <eminbay58@hotmail.com>
2021-08-07 15:48:30 +0300

    Second Commit

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
@@ -21,20 +22,50 @@ public class ProductsController {
 	
 	private ProductService productService;
 	
+	
 	@Autowired
 	public ProductsController(ProductService productService) {
 		super();
 		this.productService = productService;
 	}
 
+
 	@GetMapping("/getall")
-	public DataResult <List<Product>>getAll(){
+	public DataResult<List<Product>> getAll(){
 		return this.productService.getAll();
-	
-		
 	}
+	
 	@PostMapping("/add")
 	public Result add(@RequestBody Product product) {
 		return this.productService.add(product);
 	}
+	
+	@GetMapping("/getByProductName")
+	public DataResult<Product> getByProductName(@RequestParam String productName){
+		return this.productService.getByProductName(productName);
+	}
+	
+	@GetMapping("/getByProductNameAndCategoryId")
+	public DataResult<Product> getByProductNameAndCategoryId(@RequestParam("productName") String productName, 
+			@RequestParam("categoryId") int categoryId){
+		return this.productService.getByProductNameAndCategoryId(productName, categoryId);
+	}
+	
+	@GetMapping("/getByProductNameContains")
+	public DataResult<List<Product>> getByProductNameContains(@RequestParam String ProductName){
+		
+		return this.productService.getByProductNameContains(ProductName);
+	}
+	
+	@GetMapping("/getAllByPage")
+	public DataResult<List<Product>> getAll(int pageNo,int pageSize){
+		return this.productService.getAll(pageNo, pageSize);
+		
+	}
+	
+	@GetMapping("/getAllDesc")
+	public DataResult<List<Product>> getAllSorted() {
+		return this.productService.getAllSorted();
+	}
+	
 }
--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -5,7 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,31 +16,33 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Entity
-@Table(name = "products")
+@Table(name="products")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
-	@Id
-	@GeneratedValue(strategy =  GenerationType.IDENTITY)
-	@Column(name = "product_id")
-	private int id ;
 	
-	@Column(name = "category_id")
-	private int categoryId;
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    @Column(name="product_id")
+		private int id;
+	    
+	    //@Column(name="category_id")
+		//private int categoryId;
+	    
+	    @Column(name="product_name")
+		private String productName;
+	    
+	    @Column(name="unit_price")
+		private double unitPrice;
+	    
+	    @Column(name="units_in_stock")
+		private short unitsInStock;
+	    
+	    @Column(name="quantity_per_unit")
+		private String quantityPerUnit;
+	    
+	    @ManyToOne()
+	    @JoinColumn(name="category_id")
+	    private Category category;
 	
-	@Column(name = "product_name")
-	private String productName;
-	
-	@Column(name = "unit_price")
-	private double unitPrice;
-	
-	@Column(name = "units_in_stock")
-	private short unitsInStock;
-	
-	@Column(name = "quantity_per_unit")
-	private String quantityPerUnit;
-	
-	
-	
-
 }
